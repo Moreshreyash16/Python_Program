2b6cbd5e9cd5e8f233f3fabf580c337680b944d8
Moreshreyash16 <129577115+Moreshreyash16@users.noreply.github.com>
2023-06-07 13:54:35 +0530

    [Shreyash] Add: Python program to take user input and print

---
+++ Basic_core_pro/hello.py
@@ -0,0 +1,16 @@
+'''
+@Author: Shreyash More
+
+@Date: 2023-06-03 15:34:30
+
+@Last Modified by: Shreyash More
+
+@Last Modified time: 2023-06-05 12:34:30
+
+@Title : User Input and Replace String Template “Hello <<UserName>>, How are you?”
+
+'''
+# Taking name as input
+name=input("Enter a name \n")
+# printing the output
+print("Hello " + name + ",How are you")
